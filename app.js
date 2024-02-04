c6aab7e16a8eb011250febd9f5018e21badf4945
nmol2 <anlook009@gmail.com>
2024-02-05 02:12:44 +0530

    Adding Auth - unstable

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const cors = require('cors');
+const passport = require('passport');
 const { Person, Address } = require('./models')
 const app = express()
+
+require('./config/passport')(passport);
+// This will initialize the passport object on every request
+app.use(passport.initialize());
+// Instead of using body-parser middleware, use the new Express implementation of the same thing
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// Allows our Angular application to make HTTP requests to Express application
+app.use(cors());
+
+/**
+ * -------------- ROUTES ----------------
+ */
+// Imports all of the routes from ./routes/index.js
+app.use(require('./auth/auth-controller'));
 
 app.get('/', async function (req, res) {
     try{
